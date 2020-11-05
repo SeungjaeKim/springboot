139d3792963f74a52d4d6d8823eda6031e58764c
SeungjaeKim <kimseungjae@gmail.com>
2020-11-05 22:54:30 +0900

    test 개발 준비

---
+++ src/main/java/co/kr/springboot/controller/HelloController.java
@@ -0,0 +1,14 @@
+package co.kr.springboot.controller;
+
+import lombok.Getter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
---
+++ src/test/java/co/kr/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,46 @@
+package co.kr.springboot.domain.posts;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기(){
+
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("test@gmail.com")
+                .build());
+
+        //when
+        List<Posts> postList = postsRepository.findAll();
+
+        //then
+        Posts post = postList.get(0);
+
+
+    }
+}
