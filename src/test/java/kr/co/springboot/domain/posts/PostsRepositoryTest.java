6734507d0ef8e04f45f70a3f00b35abd7ae6c2ce
SeungjaeKim <kimseungjae@gmail.com>
2021-02-23 23:18:40 +0900

    applcation.yml을 mysql 형태로 진행하면서 에러가 h2에서 에러 나는 부분 수정

--- src/test/java/kr/co/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/kr/co/springboot/domain/posts/PostsRepositoryTest.java
@@ -4,14 +4,17 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ExtendWith({SpringExtension.class})
 @SpringBootTest
 public class PostsRepositoryTest {
 
